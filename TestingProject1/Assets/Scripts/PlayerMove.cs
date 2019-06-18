8b4665b7652dc111046a0f193b428c1acc2dadf6
Martin Chong <martinmanho@gmail.com>
2019-06-17 20:41:39 -0400

    Updated with Scoring system and enhanced collision event

---
+++ TestingProject1/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float fowardF = 2000f;
+    public float sideF = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, fowardF * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideF * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideF * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
