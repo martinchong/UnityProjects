22a0426264b681858b8bcd23ecbde98a4d96d47b
Martin Chong <martinmanho@gmail.com>
2019-06-26 16:48:40 -0400

    Renamed the Project to reflect nature of the game. Added GameOver detection and Level Restart function.

---
+++ CubeDodgerGame/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMove move;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Blocks")
+        {
+            move.enabled = false;
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+}
---
+++ CubeDodgerGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float fowardF = 2000f;
+    public float sideF = 500f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, fowardF * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideF * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideF * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1.5f)
+        {
+            FindObjectOfType<GameManager>().GameOver();
+        }
+    }
+}
