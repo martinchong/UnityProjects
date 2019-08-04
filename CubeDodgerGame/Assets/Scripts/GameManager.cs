9cb63f41aa9251862279d70b7fcebc4c5bb8faac
Martin Chong <martinmanho@gmail.com>
2019-08-03 23:50:00 -0400

    Added Level 3, Credit Scene UI and Quit button.

---
+++ CubeDodgerGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    public void Win()
+    {
+        Debug.Log("Level Clear.");
+        completeLevelUI.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart", restartDelay);
+
+        }
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
